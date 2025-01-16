1f5c1b48427b3dcd08ac9ac517d3a350ea6b9249
matsu802 <yurenagara10@gmail.com>
2025-01-16 18:04:20 +0900

    login.test.tsの追加

--- src/views/login.test.js
+++ src/views/login.test.js
@@ -4,31 +4,27 @@
 
 const ejs = require("ejs");
 beforeEach(async () => {
-    const html = await ejs.renderFile("src/views/components/telInput.ejs", {
-        tel: ""
-    });
+    const html = await ejs.renderFile("src/views/login.ejs", {});
     document.body.innerHTML = html;
 });
 
 test.each`
-    number          | label
-    ${"03-1111-2222"} | ${"市外局番ありの電話番号"}
-    ${"090-1234-1234"} | ${"携帯電話"}
+    pass          | label
+    ${"12345abc"} | ${"8文字"}
+
 `("sLabelはエラーにならない", (data) => {
-    const telInputElement = document.querySelector('input[name="tel"]');
-    telInputElement.value = data.number;
-    expect(telInputElement.validity.valid).toBe(true);
+    const passwordInputElement = document.querySelector('input[name="password"]');
+    passwordInputElement.value = data.pass;
+    expect(passwordInputElement.validity.valid).toBe(true);
 });
 
 test.each`
-    number            | label
-    ${"0311112222"}   | ${"ハイフンなし"}
-    ${"1111-2222"}    | ${"市外局番なし"}
-    ${"foobar@example.com"} | ${"メールアドレス"}
-    ${""}             | ${"空文字"}
-    ${"0900-1234-1234"} | ${"13文字以上"}
+    pass          | label
+    ${""} | ${"パスワードが空文字"}
+    ${"1"} | ${"1文字"}
+    ${"1234abc"} | ${"7文字"}
 `("sLabelはエラーになる", (data) => {
-    const telInputElement = document.querySelector('input[name="tel"]');
-    telInputElement.value = data.number;
-    expect(telInputElement.validity.valid).toBe(false);
+    const passwordInputElement = document.querySelector('input[name="password"]');
+    passwordInputElement.value = data.pass;
+    expect(passwordInputElement.validity.valid).toBe(false);
 });
